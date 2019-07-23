88297073713d90b544acab279f8f0c2474cfa2a8
Sergey Stadnik <sergey.stadnik@marketplacer.com>
2019-07-23 16:37:17 +1000

    Test the first and the last cards of the deck

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -7,4 +7,12 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 16 but got %d", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected the first card to be Ace of Spades, got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected the last card to be Four of Clubs, got %v", d[len(d)-1])
+	}
 }
