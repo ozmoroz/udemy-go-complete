afb5c06a1dfe90e96aa88cc8683efd4374f9013f
Sergey Stadnik <sstadnik@odecee.com.au>
2019-08-05 21:40:04 +1000

    Go routines project

---
+++ channels/go.mod
@@ -0,0 +1,3 @@
+module github.com/ozmoroz/udemy/go-complete/channels
+
+go 1.12
---
+++ channels/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	for _, link := range links {
+		checkLink(link)
+	}
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		return
+	}
+
+	fmt.Println(link, "is up!")
+}
